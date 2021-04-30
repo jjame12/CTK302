67d7713de44c0c1dfac8e04caa6222e659d8107b
jjame12 <70488287+jjame12@users.noreply.github.com>
2021-04-30 17:51:23 -0500

    3

--- Unit4/weather/sketch.js
+++ Unit4/weather/sketch.js
@@ -5,6 +5,8 @@ var weatherID = 0; // returned in the JSON weather element
 var myState = 0;
 var x = 0;
 var windspeed = 0 ;
+var temperature = 0 ;
+var humidity = 0 ;
 
 
 function setup() {
@@ -12,11 +14,11 @@ function setup() {
 
   // HERE is the call to get the weather.
 
-  var myCityString = 'https://api.openweathermap.org/data/2.5/weather?q=Champaign,IL,US&units=imperial&';
+  var myCityString = 'https://api.openweathermap.org/data/2.5/weather?q=chicago,IL,US&units=imperial&';
 
   //You can also use "zipcode" - var myJSONString = 'https://api.openweathermap.org/data/2.5/weather?zip=61820,us&units=imperial&';
 
-  var myIDString = 'appid=xxxxxxxx'; // USE YOUR ID HERE, take out the x's!!!
+  var myIDString = 'appid=5c349516ced4a348647e011ca1f40e83'; // USE YOUR ID HERE, take out the x's!!!
 
   var myBigString = myCityString + myIDString ;
 
@@ -45,13 +47,15 @@ function draw() {
 
     case 1:
       background(200);
-      fill('black');
+      fill('green');
       text("What is the weather in " + weather.name + "?", 20, 20);
       text("windspeed is " + windspeed, 20, 40);
+      text("temperature is " + temperature, 20, 60);
+      text("humidity is " + humidity, 20, 80);
 
 
       // cloud
-      fill('white');
+      fill('green');
       noStroke();
       ellipse(x, 300, 200, 100);
 
